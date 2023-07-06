e01b54ab76740776d5bc66b4797834686267699d
David Hsu <davidhsu@m800.com>
2023-07-06 18:09:41 +0800

    xcframework, podspec

---
+++ iOSPackageSDK.xcframework/ios-arm64/iOSPackageSDK.framework/Headers/iOSPackageSDK.h
@@ -0,0 +1,18 @@
+//
+//  iOSPackageSDK.h
+//  iOSPackageSDK
+//
+//  Created by David Hsu on 2023/7/6.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSPackageSDK.
+FOUNDATION_EXPORT double iOSPackageSDKVersionNumber;
+
+//! Project version string for iOSPackageSDK.
+FOUNDATION_EXPORT const unsigned char iOSPackageSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSPackageSDK/PublicHeader.h>
+
+
